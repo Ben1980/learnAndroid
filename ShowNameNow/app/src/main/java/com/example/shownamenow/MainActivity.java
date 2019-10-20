8e7792e4693eeda5b07da135d2aeb86de8ab3f10
Benjamin Mahr <ben.mahr@gmail.com>
2019-10-20 17:13:31 +0200

    Hello World added

---
+++ ShowNameNow/app/src/main/java/com/example/shownamenow/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.shownamenow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener((View v) -> {
+            textView.setText("Hello " + editText.getText().toString());
+        });
+
+        textView = findViewById(R.id.textView);
+    }
+}
